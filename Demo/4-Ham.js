7975a8301e2b664a6505f0e63d6b0599635e6856
nhatchimai111 <nhatchimai_n@yahoo.com>
2016-01-25 18:02:42 -0800

    + Add file 4-Ham.js

---
+++ Demo/4-Ham.js
@@ -0,0 +1,24 @@
+// Toán tử điều kiện if else
+var diem = 9;
+
+if(diem >= 5){
+
+	console.log("Dau roi");
+}else{
+
+	console.log("Rot roi");
+}
+
+// Vòng lặp for
+for (var i = 0; i < 10; i++) {
+
+	console.log("Dong so: " + i);
+}
+
+// Vòng lặp while
+var dong = 1;
+while(dong <= 5){
+
+	console.log("Line Number: " + dong);
+	dong++;
+}
