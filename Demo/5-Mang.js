b85391dbeeda40c3a6bff15fe698d7e3ec648825
nhatchimai111 <nhatchimai_n@yahoo.com>
2016-01-25 18:09:54 -0800

    + Add file 5-Mang.js

---
+++ Demo/5-Mang.js
@@ -0,0 +1,14 @@
+// Khai báo mảng
+var mang = ["Cam", "Xoai", "Chuoi"];
+//            0       1        2     Thứ tự trong mảng
+
+// Lấy phần tử trong mảng
+console.log("Lay phan tu thu 1: " + mang[1]);
+
+// Lấy số phần tử trong mảng
+console.log("So luong phan tu: " + mang.length);
+
+// Thêm phần tử vào mảng
+mang.push("Man");
+
+console.log("So luong phan tu sau khi them: " + mang.length);
