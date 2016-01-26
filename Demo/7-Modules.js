7d11b7c8b6fb74973e49f91fe688378cb273c570
nhatchimai111 <nhatchimai_n@yahoo.com>
2016-01-25 18:31:12 -0800

    + Add files export-modules.js and 7-Modules.js

---
+++ Demo/7-Modules.js
@@ -0,0 +1,7 @@
+/*Lấy giá trị được export từ module khác
+	
+	./ nghĩa là lấy đường dẫn từ node gốc
+*/
+var n = require("./export-module.js");
+
+console.log(n);
---
+++ Demo/export-module.js
@@ -0,0 +1,7 @@
+/*Export giá trị để những module khác có thể sử dụng*/
+
+// Giá trị cần export
+var name = "Trung Tam Dao Tao Tin Hoc Khoa Pham";
+
+// export 
+module.exports = name;
