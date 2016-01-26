4f0843855cf555a6bed02dd613fe9fd3611ceeb3
nhatchimai111 <nhatchimai_n@yahoo.com>
2016-01-25 17:56:52 -0800

    + Add file 3-Bien.js

---
+++ Demo/3-Bien.js
@@ -0,0 +1,25 @@
+// Khai báo kiểu số nguyên
+var n = 10;
+
+// Khai báo chuỗi
+var s = "Gia tri n = ";
+
+// Xuất giá trị ra màn hình console
+console.log(s + n);
+
+// Tính toán
+var x = 5;
+var y = 7;
+console.log(x + y);
+
+// Nối chuỗi
+var a = "5";
+var b = "7";
+
+console.log(a + b);
+
+// Ép kiểu
+a = parseInt(a);
+b = parseInt(b);
+
+console.log(a + b);
